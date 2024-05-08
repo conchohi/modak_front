01f9366ace4d72fd46e8f50c4c240b19e01d6117
conchohi <lar3242@gmail.com>
2024-05-08 15:21:56 +0900

    kakao api 구현중

---
+++ src/api/kakaoApi.js
@@ -0,0 +1,7 @@
+const rest_api_key='c97ebfa2e4bb646c4ce81908b4607956'
+const redirect_uri='http://localhost:3000/member/kakao'
+const auth_code_path='https://kauth.kakao.com/oauth/authorize'
+
+export const getKakaoLoginLink =()=>{
+    const kakaoURL = `${auth_code_path}`
+}
