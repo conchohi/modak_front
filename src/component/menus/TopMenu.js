b5ce9f6b9634a766cb7e25c1f86ff24ecc37f95e
conchohi <lar3242@gmail.com>
2024-05-10 12:03:28 +0900

    리스트 카드 추가, 상단 메뉴바 수정, 로딩 수정

--- src/component/menus/TopMenu.js
+++ src/component/menus/TopMenu.js
@@ -18,10 +18,10 @@ function TopMenu(){
     return (
         <div ref={ref} className="w-4/5 mx-auto my-5 border-b border-gray-400">
             <ul className="flex justify-start p-3 font-semibold text-lg"  onMouseOver={hoverNav} onMouseOut={outsideNav}>
-                <li className="text-center px-2"><Link className="px-5 py-1" to="/">캠핑 정보</Link></li>
-                <li className="text-center px-2"><Link className="px-5 py-1" to="/">캠핑장 목록</Link></li>
-                <li className="text-center px-2"><Link className="px-5 py-1" to="/">커뮤니티</Link></li>
-                <li className="text-center px-2"><Link className="px-5 py-1" to="/">고객지원</Link></li>
+                <li className="text-center px-3 w-1/4 md:w-1/6"><Link className="py-1" to="/">캠핑정보</Link></li>
+                <li className="text-center px-3 w-1/4 md:w-1/6"><Link className="py-1" to="/">캠핑장</Link></li>
+                <li className="text-center px-3 w-1/4 md:w-1/6"><Link className="py-1" to="/">커뮤니티</Link></li>
+                <li className="text-center px-3 w-1/4 md:w-1/6"><Link className="py-1" to="/">고객지원</Link></li>
             </ul>
             {isDropdown && <TopMenuDrop  onMouseOver={hoverNav} onMouseOut={outsideNav}/>
                 }
