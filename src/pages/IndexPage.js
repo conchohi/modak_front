53d4eda8f8bfdbf82f73bb5306eb5bae114621da
eunhye <dmsp2377@naver.com>
2024-05-22 00:47:42 +0900

    create home image

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -4,6 +4,7 @@ function IndexPage() {
   return (
     <>
       <BasicLayout>
+      <img className="w-full flex justify-center my-5" src="../img/modak_bg_home.png" alt="홈이미지"/>
         <MainComponent />
       </BasicLayout>
     </>
