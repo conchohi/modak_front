20f1bbd5f24152f5d7f7052544d7f0c39c69eea5
conchohi <lar3242@gmail.com>
2024-05-24 20:28:06 +0900

    토큰 관련 수정

--- src/api/reissue.js
+++ src/api/reissue.js
@@ -13,7 +13,37 @@ export const getAccessToken = async () => {
   
     } catch (error) {
       console.error('토큰 갱신 실패:', error);
-      throw new Error('토큰 갱신 실패');
+      alert('로그인 시간 만료, 재로그인 하세요')
+      logoutFunction();
     }
   };
   
+
+export const logoutFunction =  async () => {
+  try {
+    // 서버에 로그아웃 요청을 보냅니다.
+    const response = await axios.post('http://localhost:4040/logout', {}, {
+      withCredentials: true // 쿠키를 포함하여 요청
+    });
+
+    if (response.status === 200) {
+      // 로그아웃 성공 시, 로컬 스토리지에서 토큰 제거
+      localStorage.removeItem('access');
+      
+      // 홈으로 이동
+      window.location.href = '/';
+    } else {
+      localStorage.removeItem('access');
+      
+      // 홈으로 이동
+      window.location.href = '/';
+    }
+  } catch (error) {
+    console.error('로그아웃 오류', error);
+    localStorage.removeItem('access');
+      
+      // 홈으로 이동
+      window.location.href = '/';
+
+  }
+};
