8cf4e6bd0f0ba47f925dd96af4230103ab706ee5
conchohi <lar3242@gmail.com>
2024-05-25 00:00:19 +0900

    리뷰 등록

--- src/api/reviewApi.js
+++ src/api/reviewApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {API_SERVER_HOST} from "./campApi";
+import { getAccessToken, logoutFunction } from "./reissue";
 
 const prefix = `${API_SERVER_HOST}/api/review`;
 
@@ -21,7 +22,36 @@ export const registerReview = async (write) =>{
         }});
         return response.data;
     } catch (error) {
-        console.error("Error while fetching review list:", error);
-        throw error;
-    }
+        if(error.response.data === 'access token expired'){
+            try{
+              getAccessToken()
+            }catch(error){
+                if(error.response.data === 'refresh token expired'){
+                    alert('로그인 시간 만료, 재로그인 하세요')
+                    logoutFunction();
+                }
+            }
+        }
+    }   
+}
+    export const existByCampNo = async (campNo) =>{
+        const token = localStorage.getItem('access')
+        try {
+            const response = await axios.get(`${prefix}/exist`, {params:{campNo:campNo},headers:{
+                access : token
+            }});
+            return response.data;
+        } catch (error) {
+            if(error.response.data === 'access token expired'){
+                try{
+                  getAccessToken()
+                }catch(error){
+                    if(error.response.data === 'refresh token expired'){
+                        alert('로그인 시간 만료, 재로그인 하세요')
+                        logoutFunction();
+                    }
+                }
+            }
+        }   
+    
 }
--- src/component/list/ReviewList.js
+++ src/component/list/ReviewList.js
@@ -1,20 +1,48 @@
+import { existByCampNo } from "api/reviewApi";
+import ModalComponent from "component/common/ModalComponent";
 import Star from "component/common/Star";
 import ReviewWriteModal from "component/modal/ReviewWriteModal";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ReviewList = ({reviewList, campNo}) => {
     const [modalOpen, setModalOpen] = useState(false);
-
+    const [message, setMessage] = useState('');
+    const [warnModal, setWarnModal] = useState(false);
+    const navigate = useNavigate();
     const handleWriteButton = ()=>{
-        setModalOpen(true);
+        if(localStorage.getItem('access')){
+            existByCampNo(campNo).then(result=>{
+                console.log(result.isExist)
+                if(result.isExist){
+                    setMessage('이미 작성한 리뷰가 존재합니다.')
+                    setWarnModal(true)
+                }else{
+                    setModalOpen(true);
+                }
+            })
+        } else{
+            setMessage('로그인이 필요합니다')
+            setWarnModal(true);
+        }
     }
     const closeModal = () =>{
         setModalOpen(false)
     }
 
+    const callbackFunction = ()=>{
+        if(message == '로그인이 필요합니다'){
+            navigate('/login')
+        } else{
+            setWarnModal(false)
+        }
+    }
+    
+
     return (
         <div className="flex flex-col  m-2 mb-10">
             {modalOpen && <ReviewWriteModal campNo={campNo} close={closeModal}/>}
+            {warnModal && <ModalComponent message={message} callbackFunction={callbackFunction}/>}
             <div className="flex justify-between items-center">
                 <p className="text-xl font-semibold">캠핑 로그 ({reviewList?.length})</p>
                 <button className="px-3 py-2 bg-green-500 rounded-xl text-white" onClick={handleWriteButton}>작성하기</button>
