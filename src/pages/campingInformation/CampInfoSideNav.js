71ab509b114282925f6defed79efa45294f042ae
kimpurple0520 <kimpurple0520@gmail.com>
2024-05-24 17:09:32 +0900

    bora edit

--- src/pages/campingInformation/CampInfoSideNav.js
+++ src/pages/campingInformation/CampInfoSideNav.js
@@ -3,13 +3,13 @@ import React from "react";
 function CampInfoSideNav() {
   return (
     <div className="bg-white text-black w-30 h-full shadow-lg border border-gray-300 rounded-lg">
-      <div className="p-5 font-bold text-xl">캠핑정보</div>
-      <ul className="p-5">
+      <div className="px-5 pt-5 pb-3 p font-bold text-xl">캠핑정보</div>
+      <ul className="px-5">
         <li className="py-2 hover:bg-gray-200">
           <a href="/campinfo">캠핑 준비</a>
         </li>
         <li className="py-2 hover:bg-gray-200">
-          <a href="/">음식 추천</a>
+          <a href="/campfood">음식 추천</a>
         </li>
       </ul>
     </div>
