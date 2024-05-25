f150034e78cdcecf643c77f3dabb0cf56b90baac
conchohi <lar3242@gmail.com>
2024-05-25 21:16:16 +0900

    이메일 오류 메세지 수정

--- src/pages/Authentication/SignUp/index.tsx
+++ src/pages/Authentication/SignUp/index.tsx
@@ -187,7 +187,7 @@ export default function SignUp() {
         setEmail(value);
         if (!emailPattern.test(value)) {
             setEmailError(true);
-            setEmailMessage('영문, 숫자 포함 8자 이상 입력해주세요.');
+            setEmailMessage('이메일 형식을 지켜주세요');
         } else {
             setEmailError(false);
             setEmailMessage('');
