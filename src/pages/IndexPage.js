ceb3250ae42efdc7c2a440dccb69ae4f99ec23d6
conchohi <lar3242@gmail.com>
2024-05-25 14:28:23 +0900

    회원가입 페이지 성공 모달 추가, 메인 페이지 사진 크기 조절

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -4,7 +4,7 @@ function IndexPage() {
   return (
     <>
       <BasicLayout>
-      <img className="w-full flex justify-center my-5" src="../img/modak_bg_home.png" alt="홈이미지"/>
+      <img className="w-4/5 mx-auto flex justify-center mb-5" src="../img/modak_bg_home.png" alt="홈이미지"/>
         <MainComponent />
       </BasicLayout>
     </>
